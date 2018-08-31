7a3ad2bd110753dec0e37d0fd02f4619285289d9
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-08-30 18:20:22 -0700

    Included a function to form a product state MPS

--- normalizeMPS.jl
+++ normalizeMPS.jl
@@ -1,10 +1,37 @@
-mutable struct MPS
+mutable struct VidalMPS
     Gamma::Array
     Lambda::Array
 end
 
-a = MPS([1,2,3],[4,5,6])
+function ProductVidalMPS(ProductState,dim)
+    N = length(ProductState)
+    Hd = length(ProductState[1])
+    Gamma = Vector{Array{Array{Float64},1}}(N)
+    Lambda = Vector{Vector{Float64}(2)}(N-1)
+    for i in 1:N
+        v = ProductState[i]
+        if i == 1
+            Gammai = Vector{Vector{Float64}(dim)}(Hd)
+            for j in 1:Hd
+                vec = zeros(Float64,dim)
+                vec[1] = v[j]
+                Gammai[j] = vec
+            Gamma[i] = Gammai
+        elseif i == N
+            Gammai = Vector{Vector{Float64}(dim)}(Hd)
+            for j in 1:Hd
+                vec = zeros(Float64,dim)
+                vec[1] = v[j]
+                Gammai[j] = vec
+            Gamma[i] = Gammai
+        else
+            Gammai = Vector{Array{Float64,2}(dim,dim)}(Hd)
+            for j in 1:Hd
+                vec = zeros(Float64,dim,dim)
+                vec[1,1] = v[j]
+                Gammai[j] = vec
+            Gamma[i] = Gammai
 
-println(typeof(a))
-
-println(a.Gamma)
+        for i in 1:N-1
+            lambda = [1,0]
+            Lambda[i] = [lambda,lambda]
