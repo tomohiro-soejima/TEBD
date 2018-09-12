b1f29ac2d849c9a1d088b30ab67a657aacd48032
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-09-12 15:28:10 -0700

    make TwoGateOnMPS nested functions

--- normalizeMPS.jl
+++ normalizeMPS.jl
@@ -6,13 +6,6 @@ struct VidalMPS
     Lambda::Array{Float64,2}
 end
 
-#=
-struct VidalMPS{D::Int64,d::Int64,N::Int64}
-    Gamma::Array{Float64,4}(D,D,d,N)
-    Lambda::Array{Float64,3}(D,N+1)
-end
-=#
-
 function ProductVidalMPS(ProductState,D)
     d, N = size(ProductState)
     Gamma = zeros(Float64,D,D,d,N)
@@ -30,6 +23,7 @@ end
 
 function OneGateOnMPSCopy(MPS::VidalMPS,U,loc)
     #To figure out whether in place multiplication is faster
+    #turns out it is not much faster
     D,D2,d,N = size(MPS.Gamma)
     R = PermutedDimsArray(reshape(view(MPS.Gamma,:,:,:,loc),D^2,d),(2,1))
     R[:,:] = U*R
@@ -37,6 +31,20 @@ end
 
 function TwoGateOnMPS(MPS::VidalMPS,U,loc)
     D,D2,d,N = size(MPS.Gamma)
+    tic()
+    thetaNew = Theta_ij(MPS,U,loc,D,d)
+    toc()
+
+    tic()
+    F = LinearAlgebra.svd(copy(thetaNew))
+    toc()
+
+    tic()
+    UpdateMPSafterTwoGate(MPS,F,loc,D,d)
+    toc()
+end
+
+function Theta_ij(MPS::VidalMPS,U,loc,D,d)
     L1 = view(MPS.Lambda,:,loc)
     L2 = view(MPS.Lambda,:,loc+1)
     L3 = view(MPS.Lambda,:,loc+2)
@@ -47,14 +55,19 @@ function TwoGateOnMPS(MPS::VidalMPS,U,loc)
     for i in 1:d
         S1[(i-1)*D+1:i*D,:] = Diagonal(L1)*view(Gamma1,:,:,i)*Diagonal(L2)
     end
-    print("S1 =",S1[1,1])
     for j in 1:d
         S2[:,(j-1)*D+1:j*D] = view(Gamma2,:,:,j)*Diagonal(L3)
     end
-    print(S1*S2)
     theta = reshape(PermutedDimsArray(reshape(S1*S2,D,d,D,d),(2,4,1,3)),d^2,D^2)
-    thetaNew = reshape(PermutedDimsArray(reshape(U*theta,d,d,D,D),(3,1,4,2)),(d*D,d*D))
-    F = LinearAlgebra.svd(copy(thetaNew))
+    reshape(PermutedDimsArray(reshape(U*theta,d,d,D,D),(3,1,4,2)),(d*D,d*D))
+end
+
+function UpdateMPSafterTwoGate(MPS::VidalMPS,F,loc,D,d)
+    L1 = view(MPS.Lambda,:,loc)
+    L2 = view(MPS.Lambda,:,loc+1)
+    L3 = view(MPS.Lambda,:,loc+2)
+    Gamma1 = view(MPS.Gamma,:,:,:,loc)
+    Gamma2 = view(MPS.Gamma,:,:,:,loc+1)
     @views GL1 = PermutedDimsArray(reshape(F.U[:,1:D],D,d,D),(1,3,2))
     @views GL2 = reshape(F.Vt[1:D,:],D,D,d)
     for i in 1:D
@@ -71,10 +84,11 @@ function TwoGateOnMPS(MPS::VidalMPS,U,loc)
 end
 
 
-#=test
+
+#test
 PS = zeros(Float64,2,4)
 PS[1,:] = ones(Float64,4)
-MPS = ProductVidalMPS(PS,3)
+MPS = ProductVidalMPS(PS,5)
 
 #one site operation
 U = [1 1;1 -1]/sqrt(2)
@@ -83,4 +97,3 @@ OneGateOnMPS(MPS,U,2)
 #two site operation
 U2 = [1/sqrt(2) 0 0 1/sqrt(2);0 1 0 0; 0 0 1 0; 1/sqrt(2) 0 0 -1/sqrt(2)]
 TwoGateOnMPS(MPS,U2,3)
-=#
