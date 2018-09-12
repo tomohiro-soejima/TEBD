00a3d1b1f2804c4697d6d72ad43af13e4db17562
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-09-11 17:33:57 -0700

    remove type parameters from VidalMPS for now

--- normalizeMPS.jl
+++ normalizeMPS.jl
@@ -1,22 +1,36 @@
 using BenchmarkTools
 
-struct VidalMPS{D,d,N}
+struct VidalMPS
+    Gamma::Array{Float64,4}
+    Lambda::Array{Float64,3}
+end
+
+#=
+struct VidalMPS{D::Int64,d::Int64,N::Int64}
     Gamma::Array{Float64,4}(D,D,d,N)
     Lambda::Array{Float64,3}(D,N+1)
 end
+=#
 
 function ProductVidalMPS(ProductState,D)
     d, N = size(ProductState)
     Gamma = zeros(Float64,D,D,d,N)
     Lambda = zeros(Float64,D,D,N+1)
-    Gamma[1,1,:,:] = ProductState[:,:]
+    Gamma[1,1,:,:] = ProductState
     Lambda[1,:] = ones(Float64,N+1)
     VidalMPS(Gamma,Lambda)
 end
 
 function OneGateOnMPS(MPS::VidalMPS{D,d,N},U,loc)
-    R = PermutedDimsArray(reshape(MPS.Gamma[:,:,:,loc],D^2,d),(2,1))
+    R = PermutedDimsArray(reshape(view(MPS.Gamma,:,:,:,loc),D^2,d),(2,1))
+    R[:,:] = U*R
+end
+
+function OneGateOnMPSCopy(MPS::VidalMPS{D,d,N},U,loc)
+    #To figure out whether in place multiplication is faster
+    R = PermutedDimsArray(reshape(view(MPS.Gamma,:,:,:,loc),D^2,d),(2,1))
     R[:,:] = U*R
 end
 
 function TwoGateOnMPS(MPS::VidalMPS,U,loc)
+end
