50298fc7b4ac072916b40b16e8239ef76603ddc4
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-09-23 16:45:18 -0700

    move test codes to test.jl

--- tests.jl
+++ tests.jl
@@ -1,8 +1,38 @@
 include("./normalizeMPS.jl")
 using VidalMPSfunctions
 
+#=test for Updates
+PS = zeros(Float64,2,4)
+PS[1,:] = ones(Float64,4)
+MPS = ProductVidalMPS(PS,5)
+
+#one site operation
+U = [1 1;1 -1]/sqrt(2)
+OneGateOnMPS(MPS,U,2)
+
+#two site operation
+U2 = [1/sqrt(2) 0 0 1/sqrt(2);0 1 0 0; 0 0 1 0; 1/sqrt(2) 0 0 -1/sqrt(2)]
+TwoGateOnMPS(MPS,U2,3)
+=#
+
 zeros(Complex{Float64},2,2)
 
 PS = zeros(Float64,(2,10))
 PS[1,:] = ones(Float64,10)
 MPS = ProductVidalMPS(PS,10)
+
+Sz = [1 0; 0 -1]
+SzSz = Diagonal([1/4,-1/4,-1/4,1/4])
+function Ha(J,h,N)
+    OneSite = zeros(Float64,2,2,N)
+    for i in 1:N
+        OneSite[:,:,i] = h*Sz
+    end
+    TwoSite =zeros(Float64,4,4,N)
+    for i in 1:N-1
+        TwoSite[:,:,i] = J*SzSz
+    end
+    NNQuadHamiltonian(OneSite,TwoSite)
+end
+
+TEBD(MPS,Ha(1,0,10),1,10)
