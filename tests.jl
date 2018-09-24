987b08b144059fd685939a1678dd1ed754581d22
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-09-23 17:23:55 -0700

    add multiple @time macros for testing

--- tests.jl
+++ tests.jl
@@ -1,20 +1,7 @@
 include("./VidalTEBD.jl")
 using .VidalTEBD
 using LinearAlgebra
-
-#=test for Updates
-PS = zeros(Float64,2,4)
-PS[1,:] = ones(Float64,4)
-MPS = ProductVidalMPS(PS,5)
-
-#one site operation
-U = [1 1;1 -1]/sqrt(2)
-OneGateOnMPS(MPS,U,2)
-
-#two site operation
-U2 = [1/sqrt(2) 0 0 1/sqrt(2);0 1 0 0; 0 0 1 0; 1/sqrt(2) 0 0 -1/sqrt(2)]
-TwoGateOnMPS(MPS,U2,3)
-=#
+using BenchmarkTools
 
 function Ha(J,h,N)
     OneSite = zeros(Float64,2,2,N)
@@ -33,8 +20,9 @@ PS = zeros(Float64,(2,10))
 PS[1,:] = ones(Float64,10)
 MPS = make_productVidalMPS(PS,10)
 Sz = [1 0; 0 -1]
-SzSz = Diagonal([1/4,-1/4,-1/4,1/4])
+SzSz = LinearAlgebra.Diagonal([1/4,-1/4,-1/4,1/4])
 
 @time TEBD(MPS,Ha(1,0,10),1,10)
 @time TEBD(MPS,Ha(1,0,10),1,10)
-@btime TEBD(MPS,Ha(1,0,10),1,10)
+@time TEBD(MPS,Ha(1,0,10),1,100)
+@btime TEBD($MPS,Ha(1,0,10),1,10)
