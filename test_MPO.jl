37fdacf3fa629db6a9ccfe46f6b09524f8385709
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-09-30 16:21:31 -0700

    add more tests to test_MPO.jl

--- test_MPO.jl
+++ test_MPO.jl
@@ -2,9 +2,22 @@ include("./VidalTEBD.jl")
 using .VidalTEBD
 using LinearAlgebra
 
-N = 100
-U = LinearAlgebra.Diagonal([0,0,1,0])
+#test make_superpositionMPO
+N = 10
+U = LinearAlgebra.Diagonal([0,1,0,0])
 U2 = ones(4,N)
 U1 = reshape(U*U2,2,2,N)
-P = [exp(-(i-50)^2/50) for i in 1:100]
+P = [1 for i in 1:N]
 MPO = VidalTEBD.make_superpositionMPO(U1,P)
+
+#make all up product state
+zeros(Complex{Float64},2,2)
+PS = zeros(Float64,(2,10))
+PS[1,:] = ones(Float64,10)
+MPS = VidalTEBD.make_productVidalMPS(PS,2)
+
+#test do_MPOonMPS
+MPS2 = VidalTEBD.do_MPOonMPS(MPS,MPO)
+
+#test convert_to_Vidal
+MPS3 = VidalTEBD.convert_to_Vidal(MPS2)
