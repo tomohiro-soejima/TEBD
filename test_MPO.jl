c7044ebb2bdcae6fcba6f42f4cace76c095e6dcb
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-10-01 15:05:55 -0700

    check MPO to MPS is working properly

--- test_MPO.jl
+++ test_MPO.jl
@@ -3,7 +3,7 @@ using .VidalTEBD
 using LinearAlgebra
 
 #test make_superpositionMPO
-N = 10
+N = 4
 U = LinearAlgebra.Diagonal([0,1,0,0])
 U2 = ones(4,N)
 U1 = reshape(U*U2,2,2,N)
@@ -12,12 +12,13 @@ MPO = VidalTEBD.make_superpositionMPO(U1,P)
 
 #make all up product state
 zeros(Complex{Float64},2,2)
-PS = zeros(Float64,(2,10))
-PS[1,:] = ones(Float64,10)
+PS = zeros(Float64,(2,N))
+PS[1,:] = ones(Float64,N)
 MPS = VidalTEBD.make_productVidalMPS(PS,2)
 
 #test do_MPOonMPS
 MPS2 = VidalTEBD.do_MPOonMPS(MPS,MPO)
 
 #test convert_to_Vidal
-MPS3 = VidalTEBD.convert_to_Vidal(MPS2)
+MPS3 = VidalTEBD.convert_to_leftorthogonal(MPS2)
+MPS4 = VidalTEBD.convert_to_Vidal(MPS3)
