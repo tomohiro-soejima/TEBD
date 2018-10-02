94d745766c4fd21bb5bddddfded3328bc7932a29
Tomohiro Soejima <tomosoeoc@gmail.com>
2018-10-02 11:42:51 -0700

    do an ising_time_evolution

--- XYmodel_dynamics/isingcomputation.jl
+++ XYmodel_dynamics/isingcomputation.jl
@@ -8,18 +8,24 @@ x0 = 50
 sigma = 5
 D = 10
 T = 2*pi
-Nt = 10
+Nt = 100
 J = 1
 
 #define which variables to measure
-Sz = 1/2*[1 0;0 -1]
+Sx = 1/2*[0 1;1 0]
 O = zeros(Float64,2,2,N)
 for i in 1:N
-    O[:,:,i] = Sz
+    O[:,:,i] = Sx
 end
 
+zeros(Complex{Float64},2,2)
+PS = zeros(Float64,(2,N))
+PS[1,:] = ones(Float64,N)
+PS[:,50] = [1/sqrt(2),1/sqrt(2)]
+MPS = VidalTEBD.make_productVidalMPS(PS,D)
+
 H = ising_Hamiltonian(zeros(Float64,N),J .* ones(Float64,N-1))
-expvalues = @time excited_state_dynamics(N,x0,sigma,D,T,Nt,H,O)
+expvalues = @time VidalTEBD.getTEBDexpvalue!(MPS,H,T,Nt,O)
 x = 1:(Nt+1)
 plot(x,real(expvalues))
 savefig("isingplot.png")#
@@ -30,17 +36,16 @@ function plot_series(x,data,filename,title_name)
     for i in 1:size(data)[1]
         plot(x, real(data[i,:]),title = title_name,lw =3)
         xlabel!("Position")
-        ylabel!("1/2 - <Sz>")
+        ylabel!("<Sx>")
         ylims!((-u*1.2,u*1.2))
         savefig(filename * @sprintf("_%04d",k))
-        global k += 1
+        k += 1
     end
 end
 
-expvalues2 = -(expvalues .- 0.5)
 filename = "isingplot"
 x = 1:N
-plot_series(x,real(expvalues2),filename,"Ising plot")
+plot_series(x,real(expvalues),filename,"Ising plot")
 
 
 
